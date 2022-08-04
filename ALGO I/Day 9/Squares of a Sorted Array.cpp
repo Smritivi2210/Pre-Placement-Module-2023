283405b38fdbb24986932521174241f0cbfe91e7
Smritivi2210 <109061356+Smritivi2210@users.noreply.github.com>
2022-08-05 03:35:35 +0530

    Create Squares of a Sorted Array.cpp

---
+++ ALGO I/Day 9/Squares of a Sorted Array.cpp
@@ -0,0 +1,8 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        a=[]
+        for i in range(0,len(nums)):
+            a.append(nums[i]*nums[i])
+        a.sort()
+        return a
+        
